ce8b483d500ce1cd63fb743aab8d41f1ddf42b8e
Ken Kapptie <kkapptie@graniteschools.org>
2025-05-12 06:47:59 -0600

    Update quiz.js

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -3,6 +3,7 @@
 
 const availableSets = [
     'Short_Testing_Sample.csv',
+    'Summer_Job_Preparation_Terms_Definitions.csv',
     'Web_Development_Terms_Definitions.csv',
     'Music_Theory_Terms_Definitions.csv',
     'ARRL_Ham_Radio_General_License_Terms_Definitions.csv',
